28c7f9c440f9f09c4f97a5f7e48eb25788e93a54
giaobupai <137373766+giaobupai@users.noreply.github.com>
2024-02-07 13:26:04 +0800

    Update BatchableImmediate.java

--- common/src/main/java/net/raphimc/immediatelyfast/feature/core/BatchableImmediate.java
+++ common/src/main/java/net/raphimc/immediatelyfast/feature/core/BatchableImmediate.java
@@ -22,25 +22,3 @@ import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.RenderLayer;
 
 import java.util.Map;
-
-public class BatchableImmediate extends ImmediateAdapter {
-
-    public BatchableImmediate() {
-    }
-
-    public BatchableImmediate(final Map<RenderLayer, BufferBuilder> layerBuffers) {
-        super(layerBuffers);
-    }
-
-    public BatchableImmediate(final BufferBuilder fallbackBuffer, final Map<RenderLayer, BufferBuilder> layerBuffers) {
-        super(fallbackBuffer, layerBuffers);
-    }
-
-    @Override
-    protected void _draw(final RenderLayer layer) {
-        for (BufferBuilder bufferBuilder : this.getBufferBuilder(layer)) {
-            if (bufferBuilder != null) layer.draw(bufferBuilder, RenderSystem.getVertexSorting());
-        }
-    }
-
-}
